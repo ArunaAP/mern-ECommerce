70786ba129d93369dbc94dcca2860104ac652647
ArunaAP <it20657796@my.sliit.lk>
2023-02-23 00:10:32 +0530

    create signin API

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+  users: [
+    {
+      name: 'Aruna',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Joan',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
